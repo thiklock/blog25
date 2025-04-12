e1e9208e74c705468f288b8272c82742b720190b
thiklock <thiagoiklock@gmail.com>
2025-04-12 18:45:52 -0300

    first commit.

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})  # Directly 'post_list.html'
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post_detail.html', {'post': post})  # Directly 'post_detail.html'
